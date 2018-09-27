27dcb6a9387f6e72448f5d2c7f8b4d90a9e085fc
Javier Valdez <edgar.valdezr@gmail.com>
2018-09-27 16:44:11 -0700

    practica 8 cola, create y push

--- Practicas/practica7_pila_peek_pop_push.js
+++ Practicas/practica7_pila_peek_pop_push.js
@@ -1,27 +1,44 @@
+const readline = require('readline').createInterface({
+  	input: process.stdin,
+  	output: process.stdout
+})
+
+//SE DECLARA LA CLASE PILA CON SUS RESPECTIVOS ATRIBUTOS 
 function Pila(size) {
   this.size = size;
   this.item=[];
   this.empty=true;
   this.index=0;
 }
-
+//A LA CLASE PILA SE LE ANADE LA FUNCION PUSH CON EL PARAMETRO QUE SE DECEA GUARDAR
 Pila.prototype.push = function(item) {
+	//AQUI SE CONDICIONA PARA QUE NO SE PUEDA SEGUIR ANADIENDO SI EL INDEX SE PASA DEL TAMANO DE LA PILA
   	if(this.index!=this.size){
+  		//SE ALMACENA EL VALOR DENTRO DEL ARREGLO ITEM EN EL INDICE QUE SIGUE
   		this.item[this.index]=item;
+  		//SE LE SUMA UN AL INDICE PARA QUE EL SIGUIENTE ELEMENTO SE GUARDE EN EL SIGUIENTE ESPACIO
   		this.index+=1;
+  		//PONEMOS EN FALSE LA VARIABLE EMPTY PARA PODER SABER QUE EL ARREGLO YA NO ESTA VACIO
   		this.empty=false;
 	}else{
+		//SE MANDA UN MENSAJE DONDE DECIMOS QUE YA NO HAY ESPACIO EN LA PILA
 		console.log("no se puede agregar mas");
 	}
 };
 
+//A LA CLASE PILA SE ANADE LA FUNCION POP PARA SACAR EL ULTIMO ELEMENTO ANADIDO A LA PILA
 Pila.prototype.pop = function() {
+	//VALIDAMOS SI LA PILA NO ESTA VACIA
 	if(!this.empty){
+		//A LA VARIABLE I LA INICIALIZAMOS CON EL TAMANO ACTUAL DE LA PILA PARA SABER CUAL ES EL ULTIMO ELEMENTO
 		var i=this.item.length-1;
+		//CON LA FUNCION SPLICE ELIMINAMOS DEL ARREGLO EL INDICE INDICADO 
 		this.item.splice(i,1);
+		//SE LE RESTA UNO AL INDICE YA QUE AHORA CONTIENE UNO MENOS
 		this.index-=1;
-		console.log(this.index)
+		//SI EL INDICE ES 0
 		if(this.index==0){
+			//VOLVEMOS VERDADERA LA VARIABLE EMPTY
 			this.empty=true;
 		}
 	}else{
@@ -30,17 +47,16 @@ Pila.prototype.pop = function() {
   	
 };
 
-Pila.prototype.peek = function(opc) {
-
+Pila.prototype.peek = function() {
+	readline.question("Selecciona\n1:Peek\n2:PeekAll\n3:Peek Element\n\n", (opc) => {
 	if(opc==1){
 		console.log(this.item[this.index-1]);
+		readline.close()
 	}else if(opc==2){
 		console.log(this.item);
+		readline.close()
 	}else{
-		const readline = require('readline').createInterface({
-  			input: process.stdin,
-  			output: process.stdout
-		})
+		
 
 		readline.question("Que elemento deseas buscar?", (name) => {
   			console.log(name)
@@ -56,29 +72,24 @@ Pila.prototype.peek = function(opc) {
   				readline.close()
 		})
 		
-		
-		
 	}
+})
   	
 };
 
 
-Pila.prototype.peekAll = function() {
-  	return this.item
-};
-
+//SE CREA UN OBJETO DE LA FUNCION PILA DE UN TAMANO DE 5
 var nuevaPila=new Pila(5);
 
-
+//AQUI SE ANADE A LA PILA 5 ELEMENTOS
 nuevaPila.push("a");
 nuevaPila.push("b");
 nuevaPila.push("c");
 nuevaPila.push("1");
 nuevaPila.push("2");
-nuevaPila.push("3");
-nuevaPila.peek(1)
-nuevaPila.peek(2)
-nuevaPila.peek(3)
+
+nuevaPila.peek()
+
 
 
 
---
+++ Practicas/practica8_cola_createypush.js
@@ -0,0 +1,74 @@
+
+//SE DEFINE LA CLASE 'COLA' CON SUS RESPECTIVOS ATRUBUTOS
+function Cola(size){
+	//TAMANO DE LA COLA
+	this.size=size;
+	//ARREGLO DE ELEMENTOS
+	this.item=[];
+	//SE INICIALIZA EL INDEX CON EL TAMANO DE LA COLA MENOS UNO  
+  	this.index=size-1;
+  	//ATRIBUTO PARA SABER SI ESTA LLENA LA COLA
+  	this.full=false;
+  	//SE INICIALIZA LOS ITEMS EN 0 DEPENDIENDO DEL TAMANO INGRESADO
+  	for(var x=0;x<size; x++){
+		this.item[x]=0;
+	}
+}
+
+//SE LE AGREGA UN METODO ADD A LA CLASE COLA 
+Cola.prototype.add = function(item){
+	//SI EL ELEMENTO INGRESADO ES '0' SE MANDA UN MENSAJE DICIENDO QUE NO SE PUEDE INGRESAR EL 0
+	if(item==0){
+		console.log("No se pueden agregar 0");
+	//SI LA COLA NO ESTA LLENA SE AGREGA EL ELEMENTO
+	}else if(!this.full){
+		//SE AGREGA EL ELEMENTO EN EL INDICE CORRESPONDIENTE
+		this.item[this.index]=item;
+		//SE RESTA UNO AL INDICE
+		this.index=parseInt(this.index)-1;
+		//SI EL INDICE ES MENOR QUE 0 CAMBIAMOS EL ATRIBUTO 'FULL' A VERDADERO
+		if(this.index<0){
+			this.full=true
+		}
+		//SI LA COLA ESTA LLENA RETORNAMOS UN TRUE
+	}else{
+		return true
+	}
+}
+
+//SE IMPORTA EL MODULO READLINE DE NODEJS PARA LEER  DESDE LA CONSOLA
+const readline = require('readline').createInterface({
+  	input: process.stdin,
+  	output: process.stdout
+})
+
+
+
+//SE PREGUNTA EL TAMANO DE LA COLA Y SE ALMACENA EL VALOR EN LA VARIABLE SIZE
+readline.question("Inserta el tamano de la cola\n", (size) => {
+	//SE CREA UN OBJETO DE LA CLASE COLA DEL TAMANO INGRESADO
+	var cola= new Cola(size);
+	//SE IMPRIME LA COLA VACIA
+	console.log("Tu cola",cola.item)
+	//SE LLAMA UNA FUNCION RECURSIVA PARA ANADIR ELEMENTOS Y SE LE PASA COMO PARAMETRO EL OBJETO 'COLA'
+	addRecursivo(cola);
+	
+});
+
+//FUNCION RECURSIVA PARA PEDIR LA INSERCION DE ELEMENTOS 
+var addRecursivo= function (cola) {
+	//SE PREGUNTA EL ELEMENTO QUE SE DESEA ANADIR A LA COLA
+  readline.question('"Inserta el elemento\n: ', function (item) {
+  	//CONDICION PARA SALIR DEL MENU ESCRIBIENDO 'exit' EN LA CONSOLA
+    if (item == 'exit') 
+      return readline.close(); 
+
+  	//SI AL ANADIR EL ITEM RETORNA VALOR DE VERDADERO MANDA EL MENSAJE DE COLA LLENA
+    if(cola.add(item)){
+    	console.log("cola llena")
+    //SI NO SE VUELVE A LLAMAR LA FUNCION PASANDOLE EL OBJETO COMO PARAMETRO
+    }else{
+    	addRecursivo(cola);
+    } 
+  });
+};
