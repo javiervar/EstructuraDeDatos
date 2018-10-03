20bad10bf54d8f4036f9aa58511e0c871e135b66
Javier Valdez <edgar.valdezr@gmail.com>
2018-10-02 18:33:30 -0700

    practica 8 completa con comentarios

--- Practicas/practica8_cola_peek.js
+++ Practicas/practica8_cola_peek.js
@@ -28,6 +28,7 @@ Cola.prototype.add = function(item) {
         //SI EL INDICE ES MENOR QUE 0 CAMBIAMOS EL ATRIBUTO 'FULL' A VERDADERO
         if (this.index < 0) {
             this.full = true
+            this.index=0;
         }
         //SI LA COLA ESTA LLENA RETORNAMOS UN TRUE
     } else {
@@ -66,6 +67,56 @@ Cola.prototype.pop = function() {
     }
 }
 
+//SE AGREGA EL METODO PEEK A LA CLASE COLA
+Cola.prototype.peek = function() {
+    //SE COMPRUEBA SI LA COLA ESTA VACIA, SI LO ESTA DEVUELVE 'COLA VACIA' Y SI NO DEVUELVE EL VALOR DE LA ULTIMA POSICION AGREGADA
+    if(this.index==this.size-1){
+        console.log("cola vacia")
+    }
+    else{
+        //SE IMPRIME EL ULTIMO VALOR AGREGADO A LA COLA
+        console.log(this.item[this.index]); 
+    }
+}
+//SE AGREGA EL METODO PEEKALL A LA CLASE COLA
+Cola.prototype.peekAll = function() {
+    //SE COMPRUEBA SI LA COLA ESTA VACIA, SI LO ESTA DEVUELVE 'COLA VACIA' Y SI NO DEVUELVE TODOS LOS ELEMENTOS DE LA COLA
+
+    if(this.index==this.size-1)
+        console.log("cola vacia")
+    else
+        console.log(this.item);
+}
+
+//SE AGREGA EL METODO PEEKELEMENT A LA CLASE COLA EL CUAL RECIBE EL PARAMETRO QUE SE BUSCARA
+Cola.prototype.peekElement = function(elemento) {
+    //BANDERA PARA COMPROBAR LA EXISTENCIA DEL ELEMENTO
+    var flag=true;
+    //CICLO PARA ENCONTRAR EL ELEMENTO A BUSCAR DENTRO DE LOS ELEMENTOS DE LA COLA
+    for(var x in this.item){
+        //SE COMPARA EL ELEMENTO ACTUAL DEL FOR CON ELE ELEMENTO A BUSCAR
+        //SI SON IGUALES SE IMPRIME EL INDICE Y EL ELEMENTO BUSCADO Y LA BANDERA CAMBIA A FALSO
+        if(this.item[x]==elemento){
+            console.log({index:x,elemento:elemento});
+            flag=false;
+        }
+    }
+    //SI LA BANDERA ES VERDADERA SE IMPRIME MENSAJE DE QUE NO EXISTE EL ELEMENTO
+    if(flag){
+        console.log("No existe el elemento buscado")
+    }
+}
+
+//SE AGREGA EL METODO PEEKNEXT A LA CLASE COLA
+Cola.prototype.peekNext = function() {
+    //SE COMPRUEBA SI LA COLA ESTA VACIA, SI LO ESTA DEVUELVE 'COLA VACIA' Y SI NO DEVUELVE EL ELEMENTO PROXIMO A SALIR
+    if(this.item[this.size-1]==0)
+        console.log("la cola esta vacia")
+    else
+        console.log(this.item[this.size-1]);
+}
+
+
 
 //AUI EMPIEZA TODO EL MENU
 
@@ -89,9 +140,9 @@ readline.question("Inserta el tamano de la cola\n", (size) => {
 //FUNCION RECURSIVA PARA PODER ELEGIR SI VA AGREGAR O QUITAR DE LA COLA
 var menuRecursivo = function(cola) {
     //SE PREGUNTA LA OPCION A ELEGIR
-    readline.question('"Elige una opcion:\n1:push\n2:pop\n3:exit\n:', function(opc) {
+    readline.question('"Elige una opcion:\n1:push\n2:pop\n3:peek:\n4:exit\n:', function(opc) {
         //OPCION 3 CONDICION PARA SALIR DEL MENU
-        if (opc == 3){
+        if (opc == 4){
         	//SE CIERRA EL READLINE
             return readline.close();
         }
@@ -107,13 +158,47 @@ var menuRecursivo = function(cola) {
             //SE IMPRIME EL ARREGLO DE ITEMS DENTRO DE LA COLA
             console.log(cola.item)
         }
+        else if (opc == 3) {
+            peekRecursivo(cola);
+        }
 
         //SE VUELVE A LLAMAR EL MENU RECURSIVO PASANDO COMO PARAMETRO EL OBJETO COLA
         menuRecursivo(cola);
     });
 };
 
+//MENU PARA LAS OPCIONES DEL METODO PEEK
+var peekRecursivo = function(cola) {
+    //SE PREGUNTA QUE OPCION DE PEEK SEA DESEA UTILIZAR
+    readline.question('Elige una opcion:\n 1:Peek\n 2:Peek All\n 3:Peek Element\n 4:Peek Next\n 5:exit\n : ', function(item) {
+        //CONDICION PARA SALIR DEL MENU PEEK ESCRIBIENDO '5' EN LA CONSOLA
+        if (item == 5)
+            menuRecursivo(cola)
 
+        //SI ESCOGE 1 SE LLAMA EL METODO PEEK DE LA COLA
+        if(item==1){
+            cola.peek();
+        }else if(item==2){//SI ESCOGE 2 SE LLAMA AL METODO PEEK ALL DE LA COLA
+            cola.peekAll();
+        }
+        else if(item==3){//SI SE ESCOGE 3 SE LLAMA AL MENU DE PEEK ELEMENTO
+            peekElementRecursivo(cola);
+        }
+         else if(item==4){//SI SE ESCOGE 4 SE LLAMA AL METODO PEEK NEXT DE LA COLA
+            cola.peekNext()
+        }
+        //SE VUELVE A LLAMAR ASI MISMO EL METODO PARA VOLVER A ELEGIR UNA OPCION
+        peekRecursivo(cola)
+    });
+};
+
+//FUNCION PARA PREGUNTAR ELEMENTO A BUSCAR DENTRO DE LA COLA EL CUAL RECIVE EL OBJETO COLA
+var peekElementRecursivo=function(cola){
+    readline.question('Que elemento deseas buscar?\n: ', function(item) {
+        cola.peekElement(item);//SE LLAMA AL METODO DE PEEK ELEMENT DE LA COLA PASANDOLE COMO PARAMETRO EL ELEMENTO QUE SE DESEA BUSCAR
+        peekRecursivo(cola);//SE REGRESA AL MENU DE PEEK
+    });
+}
 
 //FUNCION RECURSIVA PARA PEDIR LA INSERCION DE ELEMENTOS 
 var addRecursivo = function(cola) {
@@ -129,7 +214,7 @@ var addRecursivo = function(cola) {
             menuRecursivo(cola)
             //SI NO SE VUELVE A LLAMAR LA FUNCION PASANDOLE EL OBJETO COMO PARAMETRO
         } else {
-        	console.log(cola.item)
+        	console.log(cola.item);
             addRecursivo(cola);//LA FUNCION SE LLAMA A SI MISMA PARA VOLVER A PREGUNTAR
         }
     });
